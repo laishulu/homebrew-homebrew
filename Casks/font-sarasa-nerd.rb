3424f68132c8f7942175790a4b8304b04d8ed1ec
Mao <code@mao.to>
2024-11-03 13:19:21 +0800

    fix asset url

--- Casks/font-sarasa-nerd.rb
+++ Casks/font-sarasa-nerd.rb
@@ -2,7 +2,7 @@ cask "font-sarasa-nerd" do
   version "1.1.0"
   sha256 "57028dc7e0c58fa2e0aa41b8a4c2a060925f3d6c0877d737d4516661a676dfe4"
 
-  url "https://github.com/laishulu/Sarasa-Term-SC-Nerd/releases/download/v#{version}/sarasa-term-sc-nerd.ttc.tar.gz"
+  url "https://github.com/laishulu/Sarasa-Term-SC-Nerd/releases/download/v#{version}/SarasaTermSCNerd.ttc.tar.gz"
   name "Sarasa Term SC Nerd font"
   homepage "https://github.com/laishulu/Sarasa-Term-SC-Nerd"
 
