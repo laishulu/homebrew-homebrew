2aa8ce5a415834e07bb1a4db118ff179a505f6cf
Mao <code@mao.to>
2024-11-03 22:30:43 +0800

    update hash

--- Casks/font-sarasa-nerd.rb
+++ Casks/font-sarasa-nerd.rb
@@ -4,7 +4,7 @@ cask "font-sarasa-nerd" do
   version "2.0.0"
 
   url "https://github.com/laishulu/Sarasa-Term-SC-Nerd/releases/download/v#{version}/SarasaTermSCNerd.ttc.tar.gz"
-  sha256 "ad6c5ac23952ad9751e5ea91b509a7993bdeb739340ef2c1c6a34d5b80f3975e"
+  sha256 "027ff42b3f0cedf8686fdbf3a833c28dc2b2144519d5c753444df9a66de16bbe"
 
   font "SarasaTermSCNerd.ttc"
 end
