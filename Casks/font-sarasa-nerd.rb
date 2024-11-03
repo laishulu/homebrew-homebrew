6dcad6aa2cd06beb75ff5369cbfc93641e31e108
Mao <code@mao.to>
2024-11-03 22:02:16 +0800

    fix cask

--- Casks/font-sarasa-nerd.rb
+++ Casks/font-sarasa-nerd.rb
@@ -1,10 +1,10 @@
 cask "font-sarasa-nerd" do
-  version "1.1.0"
-  sha256 "57028dc7e0c58fa2e0aa41b8a4c2a060925f3d6c0877d737d4516661a676dfe4"
-
-  url "https://github.com/laishulu/Sarasa-Term-SC-Nerd/releases/download/v#{version}/SarasaTermSCNerd.ttc.tar.gz"
   name "Sarasa Term SC Nerd font"
   homepage "https://github.com/laishulu/Sarasa-Term-SC-Nerd"
+  version "2.0.0"
 
-  font "sarasa-term-sc-nerd.ttc"
+  url "https://github.com/laishulu/Sarasa-Term-SC-Nerd/releases/download/v#{version}/SarasaTermSCNerd.ttc.tar.gz"
+  sha256 "57028dc7e0c58fa2e0aa41b8a4c2a060925f3d6c0877d737d4516661a676dfe4"
+
+  font "SarasaTermSCNerd.ttc"
 end
